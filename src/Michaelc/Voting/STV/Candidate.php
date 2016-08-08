7113a6411c953a3a8c77df5bda8d00aa2e1a73b5
MichaelC <m@michaelcullum.com>
2016-08-08 01:03:04 +0100

    Add new tests and bulk out election object

--- src/Michaelc/Voting/STV/Candidate.php
+++ src/Michaelc/Voting/STV/Candidate.php
@@ -8,15 +8,124 @@ class Candidate
   	const RUNNING = 2;
   	const DEFEATED = 3;
 
+  	/**
+  	 * Identifier for the candidate
+  	 *
+  	 * @var integer
+  	 */
   	protected $id;
+
+  	/**
+  	 * Number of votes the candidate currently has
+  	 *
+  	 * @var float
+  	 */
 	protected $votes;
+
+	/**
+	 * Number of surplus votes the candidate has to be re-allocated
+	 *
+	 * @var float
+	 */
 	protected $surplus;
+
+	/**
+	 * State of the candidate (use class constants)
+	 *
+	 * @var integer
+	 */
 	protected $state;
 
-	function __construct()
+	/**
+	 * Constructor
+	 */
+	public function __construct(int $id)
 	{
-		$this->votes = 0;
-		$this->surplus = 0;
+		$this->id = $id;
+		$this->votes = 0.0;
+		$this->surplus = 0.0;
 		$this->state = self::RUNNING;
 	}
+
+    /**
+     * Gets the Identifier for the candidate.
+     *
+     * @return integer
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Gets the Number of votes the candidate currently has.
+     *
+     * @return float
+     */
+    public function getVotes(): float
+    {
+        return $this->votes;
+    }
+
+    /**
+     * Sets the Number of votes the candidate currently has.
+     *
+     * @param float $votes the votes
+     *
+     * @return self
+     */
+    protected function setVotes(float $votes)
+    {
+        $this->votes = $votes;
+
+        return $this;
+    }
+
+    /**
+     * Gets the Number of surplus votes the candidate has to be re-allocated.
+     *
+     * @return float
+     */
+    public function getSurplus(): float
+    {
+        return $this->surplus;
+    }
+
+    /**
+     * Sets the Number of surplus votes the candidate has to be re-allocated.
+     *
+     * @param float $surplus the surplus
+     *
+     * @return self
+     */
+    protected function setSurplus(float $surplus)
+    {
+        $this->surplus = $surplus;
+
+        return $this;
+    }
+
+    /**
+     * Gets the State of the candidate (use class constants).
+     *
+     * @return integer
+     */
+    public function getState(): int
+    {
+        return $this->state;
+    }
+
+    /**
+     * Sets the State of the candidate (use class constants).
+     *
+     * @param integer $state the state
+     *
+     * @return self
+     */
+    protected function setState(int $state)
+    {
+        $this->state = $state;
+
+        return $this;
+    }
 }
--- src/Michaelc/Voting/STV/Election.php
+++ src/Michaelc/Voting/STV/Election.php
@@ -2,12 +2,36 @@
 
 namespace Michaelc\Voting\STV;
 
+use Michaelc\Voting\STV\Candidate;
+
 class Election
 {
+	/**
+	 * Count of candidates in election
+	 *
+	 * @var int
+	 */
 	protected $candidateCount;
+
+	/**
+	 * Count of number of seats/winners
+	 *
+	 * @var int
+	 */
 	protected $winnersCount;
 
+	/**
+	 * Array of candidates competing in election
+	 *
+	 * @var array
+	 */
 	protected $candidates;
+
+	/**
+	 * Array of ballots cast in election
+	 *
+	 * @var array
+	 */
 	protected $ballots;
 
 	public function __construct(int $winnersCount, array $candidates, array $ballots)
@@ -15,5 +39,66 @@ class Election
 		$this->winnersCount = $winnersCount;
 		$this->candidates = $candidates;
 		$this->ballots = $ballots;
+		$this->candidateCount = count($candidates);
 	}
+
+	public function getCandidate(int $id): Candidate
+	{
+		return $this->candidates[$id];
+	}
+
+	public function getCandidateCount(): int
+	{
+		return $this->candidateCount;
+	}
+
+	public function getActiveCandidates(): array
+	{
+		$activeCandidates = [];
+
+		foreach ($this->candidates as $candidateId => $candidate)
+		{
+			if ($candidate->getState() == Candidate::RUNNING)
+			{
+				$activeCandidates[$candidateId] = $candidate;
+			}
+		}
+
+		return $activeCandidates;
+	}
+
+	public function getActiveCandidateCount(): int
+	{
+		return count($this->getActiveCandidates());
+	}
+
+    /**
+     * Gets the value of winnersCount.
+     *
+     * @return int
+     */
+    public function getWinnersCount(): int
+    {
+        return $this->winnersCount;
+    }
+
+    /**
+     * Gets the value of candidates.
+     *
+     * @return array
+     */
+    public function getCandidates(): array
+    {
+        return $this->candidates;
+    }
+
+    /**
+     * Gets the value of ballots.
+     *
+     * @return array
+     */
+    public function getBallots(): array
+    {
+        return $this->ballots;
+    }
 }
