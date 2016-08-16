ce3c7910bd28e1221a5be37d7c7e0892202fcd82
MichaelC <m@michaelcullum.com>
2016-08-16 13:56:17 +0100

    Test fully the candidate methods

--- tests/Michaelc/Voting/StvCandidateTest.php
+++ tests/Michaelc/Voting/StvCandidateTest.php
@@ -14,4 +14,31 @@ class StvCandidateTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($candidate->getVotes(), 0.0);
         $this->assertEquals($candidate->getState(), Candidate::RUNNING);
     }
+
+    public function testCandidateStates()
+    {
+        $candidate = new Candidate(random_int(0,10));
+
+        $candidate->setState(Candidate::ELECTED);
+        $this->assertEquals($candidate->getState(), Candidate::ELECTED);
+
+        $candidate->setState(Candidate::RUNNING);
+        $this->assertEquals($candidate->getState(), Candidate::RUNNING);
+
+        $candidate->setState(Candidate::DEFEATED);
+        $this->assertEquals($candidate->getState(), Candidate::DEFEATED);
+    }
+
+    public function testCandidateVotes()
+    {
+        $candidate = new Candidate(random_int(0,10));
+
+        $this->assertEquals($candidate->getVotes(), 0.0);
+
+        $candidate->addVotes(4);
+        $this->assertEquals($candidate->getVotes(), 4.0);
+
+        $candidate->addVotes(0.11112);
+        $this->assertEquals($candidate->getVotes(), 4.11112);
+    }
 }
